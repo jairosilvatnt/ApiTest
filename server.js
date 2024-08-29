b2a8b1b31e935d0bdf98d1d11a9fc86b4a9359a0
Jairo da Silva <silva_jairo2009@yahoo.com.br>
2024-08-29 00:38:59 -0300

    coneçao prisma

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json())
 
 const users = []
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
 
-  // res.send(req.body)
-  users.push(req.body)
-  res.send("Esta postagem esta funcionando corretamente!")
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age
+    }
+  })
 })
 
 app.get('/usuarios', (req, res) => {
