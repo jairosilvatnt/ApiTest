69f49c670281fed033af10f6b2f60c39cd8230b3
Jairo da Silva <silva_jairo2009@yahoo.com.br>
2024-08-28 22:47:18 -0300

    commit seguinte

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 import express from "express";
 
 const app = express();
+app.use(express.json())
 
-app.get('/teste', (req, res) => {
-  res.send("testando esta rota agora!")
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+  // res.send(req.body)
+  users.push(req.body)
+  res.send("Esta postagem esta funcionando corretamente!")
 })
 
+app.get('/usuarios', (req, res) => {
+  res.json(users)
+  res.send("Esta postagem esta funcionando corretamente!")
+
+})
+
+
+
 app.listen(3000)
+
+/*
+    Passo a passo dentro da construção da minha API;
+      -(POST) Criar um usuário
+      -(GET) Listar todos os usuários
+      -(GET/{ID}) Pegar usuário pelo ID
+      -(PUT/{ID}) Atualizar usuário pelo ID
+      -(DELET/{ID}) Deletar usuário pelo ID
+*/
+
+/*
+ TIPOS DE ERROS 
+ 2XX -> SUCESSO
+ 4XX -> ERRO NO FRONT-END (CLIENTE)
+ 5XX -> ERRO NO SERVIDOR
+*/
